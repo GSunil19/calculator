edd178f65706819ab7c56f73761a481e768edd9b
GSunil19 <66327964+GSunil19@users.noreply.github.com>
2020-06-02 11:35:31 -0700

    Updated spacess
    Cleanup unnecessary spaces.

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,7 @@ exports.calculate = function(req, res) {
 
   var operations = {
      // Using + operator to coerce variables to numbers to avoid string concatination.
-    'add':      function(a,b) { return +a + +                                   b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
@@ -33,7 +33,7 @@ exports.calculate = function(req, res) {
   // Validate operands
 
   if (! req.query.operand1 ||
-      ! req.query.operand1.mat                                                  ch(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+      ! req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
       req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
     throw new Error("Invalid operand1: " + req.query.operand1);
   }
