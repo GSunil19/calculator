9c61e32a26a447ed201054fc4f60819fdc15cfbd
GSunil19 <66327964+GSunil19@users.noreply.github.com>
2020-06-02 11:15:58 -0700

    Update addition
    Removed unnecessary plus signs

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -1,6 +1,6 @@
-'use strict';
+        'use strict';
 
-exports.calculate = function(req, res) {
+exports.calculate = function(req, res) {              
   req.app.use(function(err, req, res, next) {
     if (res.headersSent) {
       return next(err);
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return +a + +b },
+    'add':      function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
